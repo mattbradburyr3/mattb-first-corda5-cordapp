ac46e1f93342311c49f90d8a269159a573d16025
mattbradburyr3 <matthew.bradbury@r3.com>
2022-06-29 12:47:14 +0100

    HelloWorldFlow working

--- src/main/kotlin/com/mattb/cordapp/helloworld/DummyFlow.kt
+++ src/main/kotlin/com/mattb/cordapp/helloworld/DummyFlow.kt
@@ -1,7 +1,8 @@
-package com.`mattb-first-corda5-cordapp`
+package com.mattb.cordapp.helloworld
 
 import net.corda.v5.application.flows.RPCRequestData
 import net.corda.v5.application.flows.RPCStartableFlow
+import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.util.contextLogger
 
 
@@ -11,8 +12,9 @@ class HelloWorldFlow: RPCStartableFlow {
         val log = contextLogger()
     }
 
+    @Suspendable
     override fun call(requestBody: RPCRequestData): String {
         log.info("MB: Hello world!")
-        return "this call function returns Hello World"
+        return "this call function returns Hello World, but is the version for Conal"
     }
 }
