5834655ed2da036dbb61863882a44ce988c527d5
mattbradburyr3 <matthew.bradbury@r3.com>
2022-06-30 17:05:07 +0100

    Basic PassAMessageFlow passing a message between two VNs

--- src/main/kotlin/com/mattb/cordapp/helloworld/PassAMessageFlow.kt
+++ src/main/kotlin/com/mattb/cordapp/helloworld/PassAMessageFlow.kt
@@ -1,27 +1,63 @@
 package com.mattb.cordapp.helloworld
 
 import net.corda.v5.application.flows.*
+import net.corda.v5.application.marshalling.JsonMarshallingService
+import net.corda.v5.application.messaging.FlowMessaging
 import net.corda.v5.application.messaging.FlowSession
+import net.corda.v5.application.messaging.unwrap
+import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.annotations.Suspendable
+import net.corda.v5.base.types.MemberX500Name
+import net.corda.v5.base.util.contextLogger
 
-
-@InitiatingFlow("pass-a-message=protocol")
+@InitiatingFlow("pass-a-message-protocol")
 class PassAMessageFlow: RPCStartableFlow {
+
+    private companion object {
+        val log = contextLogger()
+    }
+
+    @CordaInject
+    lateinit var jsonMarshallingService: JsonMarshallingService
+
+    @CordaInject
+    lateinit var flowMessaging: FlowMessaging
+
 
     @Suspendable
     override fun call(requestBody: RPCRequestData): String {
-        TODO("Not yet implemented")
+        log.info("MB: PassAMessageFlow.call() called")
+
+
+        val myMessageDTO = requestBody.getRequestBodyAs(jsonMarshallingService, MyMessageDTO::class.java)
+        val recipient = MemberX500Name.parse(myMessageDTO.recipientX500)
+
+        log.info("MB: myMessageDTO in Initiator: $myMessageDTO")
+
+        val session = flowMessaging.initiateFlow(recipient)
+        //        session.sendAndReceive(MyMessageDTO::class.java, payload )
+        session.send(myMessageDTO)
+        return "MB: recipient: $recipient"
     }
 }
-
 
 @InitiatedBy("pass-a-message-protocol")
 class PassAMessageResponderFlow: ResponderFlow {
 
-    @Suspendable
-    override fun call(session: FlowSession) {
-        TODO("Not yet implemented")
+    private companion object {
+        val log = contextLogger()
     }
 
-}
+    @Suspendable
+    override fun call(session: FlowSession) {
+        log.info("MB: PassAMessageResponderFlow.call() called")
 
+//        val info = session.getCounterpartyFlowInfo()
+//        log.info("MB: info in Responder: $info")
+
+        val myMessageDTO = session.receive(MyMessageDTO::class.java).unwrap { it }
+        log.info("MB: myMessageDTO in Responder: $myMessageDTO")
+    }
+}
+@CordaSerializable
+data class MyMessageDTO(val recipientX500: String, val message: String)
--- src/main/kotlin/com/mattb/cordapp/helloworld/ReceiveDataFromRPCFlow.kt
+++ src/main/kotlin/com/mattb/cordapp/helloworld/ReceiveDataFromRPCFlow.kt
@@ -5,6 +5,7 @@ import net.corda.v5.application.flows.RPCRequestData
 import net.corda.v5.application.flows.RPCStartableFlow
 import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.base.annotations.Suspendable
+import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.base.util.contextLogger
 
 class ReceiveDataFromRPCFlow: RPCStartableFlow {
@@ -24,7 +25,10 @@ class ReceiveDataFromRPCFlow: RPCStartableFlow {
         log.info("MB: dataString: $dataString")
         val data = requestBody.getRequestBodyAs(jsonMarshallingService, MyRequestData::class.java )
         log.info("MB: data: $data")
-        val compResult = "${data.myString} ${data.myInteger}"
+        val x500 = MemberX500Name.parse(data.myX500)
+        log.info("MB: x500: $x500")
+
+        val compResult = "${data.myString} ${data.myInteger} $x500"
         log.info("MB: compResult: $compResult")
         return "MB: compResult: $compResult"
     }
@@ -32,5 +36,6 @@ class ReceiveDataFromRPCFlow: RPCStartableFlow {
 
 data class MyRequestData(
     val myString: String,
-    val myInteger: Int
+    val myInteger: Int,
+    val myX500: String
         ) {}
